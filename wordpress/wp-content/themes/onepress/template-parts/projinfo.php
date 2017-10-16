be568be5fcb459493c1cd6d2f9a320dd535923d2
AnchoR29 <31813688+AnchoR29@users.noreply.github.com>
2017-10-16 18:57:28 +0800

    Database Integ

--- wordpress/wp-content/themes/onepress/template-parts/projinfo.php
+++ wordpress/wp-content/themes/onepress/template-parts/projinfo.php
@@ -4,7 +4,7 @@
 	 * @link https://codex.wordpress.org/Template_Hierarchy
 	 * @package OnePress
 	 */
-	if (isset($_GET['view']))
+	if (isset($_GET['view']))		
 		$proj_title = htmlspecialchars($_GET['view']);
 ?>
 
@@ -24,7 +24,7 @@
 			$proj_info = $result['proj_info'];
 		}
 	$proj_info = str_replace("\n", "<br><br>", $proj_info);			//TEXT PARAGRAPH LAYOUT
-
+	
 	#Header Setup
 	get_header();
 	$layout = onepress_get_layout();
@@ -34,7 +34,7 @@
 <link href="../wp-content/themes/onepress/assets/css/projstyles.css" type="text/css" rel="stylesheet" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<div id="goal">
+	<div id="goal">		
 		<div id="contentgoal">
 			<?php 	if(IsSet($proj_title))	echo "<p><h2>$proj_title</h2></p>";
 					else 					echo "<h2>Project name not found</h2>";
@@ -58,11 +58,11 @@
 							if($user_comment != ''){
 								if($list['anon'] == 1)	$pledger = "Anonymous";
 								else 					$pledger = $list['user'];
-								echo "<li>$pledger</li>";
-								echo "($action_date): ";
+								echo "<li>$pledger</li>";	
+								echo $action_date;
 								echo $user_comment;
 							}
-						}
+						}					
 					else echo "<p>No comments yet!</p>";
 					echo "</div>";
 			?>
@@ -71,13 +71,13 @@
 		<div id="sidebarprojinfo">
 			<div id="asidegoal" style="color:black; font-size: 15px; letter-spacing:1px;">
 				<p><b>Goal PHP</b></p>
-				<?php
+				<?php 
 					if(IsSet($proj_goal))	{echo "<span>P</span>";
 											echo "<span style='float:right; letter-spacing:2px;'>".number_format($proj_goal)."</span>";}
 					else 					echo "<p>Goal amount not set</p>";
-				?>
+				?>		
 				<br><p><b>Raised PHP</b></p>
-				<?php
+				<?php 
 					if(IsSet($proj_fund))	{echo "<span>P</span>";
 											echo "<span style='float:right; letter-spacing:2px;'>".number_format($proj_fund)."</span>";}
 					else 					echo "<p>Amount raised not defined</p>";
@@ -89,15 +89,15 @@
 				<?php
 				if (!is_user_logged_in() ){
 					echo "<p style='text-transform:none; text-align:center; color:black;'>
-						You need to be a registered user to donate. Click here to
-						<a href='http://localhost/wordpress/signup/'><strong>register</strong></a> or
+						You need to be a registered user to donate. Click here to 
+						<a href='http://localhost/wordpress/signup/'><strong>register</strong></a> or 
 						<a href='http://localhost/wordpress/login/''><strong>sign in</strong></a>.
 						</p>";
 				} else {
 					$current_user = wp_get_current_user();
 					$query = "SELECT SUM(fund_given) FROM user_actions WHERE proj_title='$proj_title' AND user='$current_user->user_login'";
 					$user_donate = $wpdb->get_var($query);
-					if(IsSet($user_donate))	echo "<p style='text-align:center; color:#014421; font-size:13px;'><strong>WANT TO DONATE AGAIN?</strong></p>";
+					if(IsSet($user_donate))	echo "<p style='text-align:center; color:#014421; font-size:13px;'><strong>WANT TO DONATE AGAIN?</strong></p>";	
 					else 	$user_donate = 0;
 					echo "<p style='text-align:center; color:#7b1113; font-size: 12px;'>You currently have pledged P$user_donate in the project!</p>";
 					echo "<form action='pledge-processing' method='post' style='color:#7b1113;'>
@@ -105,7 +105,7 @@
 						        <label for='pledge'><strong>PLEDGE AMOUNT:</strong></label>
 						        <input type='number' id='pledge' name='pledge_amount' min='1' style='width:100%;' required>
 						        <br><label for='comment'><strong>ANY COMMENTS? (Optional)</strong></label>
-        						<textarea id='comment' name='user_comment' style='width:100%;'></textarea>
+        						<textarea id='comment' name='user_comment' style='width:100%;'></textarea>	
 						    <div style='font-size:10px;'>
 						    	<input type='checkbox' name='Anonymous' value='Yes'/>Check this box if you want to be Anonymous :)
 							</div>
@@ -121,14 +121,14 @@
 								if($list['anon'] == 1)	$pledger = "Anonymous";
 								else 					$pledger = $list['user'];
 								echo "<li>$pledger</li>";
-								//$pledge_amount = $list['fund_given'];  #OPTIONAL PLEDGE AMOUNT DISPLAY
-							}
+								//$pledge_amount = $list['fund_given'];  #OPTIONAL PLEDGE AMOUNT DISPLAY  
+							}					
 						else echo "<p>Be the first to pledge!</p>";
 				?></ul>
 			</div>
 		</div>
 	</div>
-
+	
 <footer style="clear:both;display: block">
 	<?php get_footer();?>
 </footer>
